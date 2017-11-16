7f3795009ba39c5b8ec005de264d58de31163547
Federico Bolelli <lk_number1@hotmail.it>
2017-11-16 14:50:52 +0100

    Update conversion from tree to dag

--- src/main.cpp
+++ src/main.cpp
@@ -8,124 +8,17 @@
 #include <map>
 #include <unordered_map>
 #include <unordered_set>
-#include <memory>
 
+#include "condition_action.h"
 #include "hypercube.h"
+#include "output_generator.h"
+#include "tree.h"
 
 using namespace std;
 
 #define LOG(message, instructions) cout << (message) << "... "; instructions cout << "done.\n"
 
-template<typename T>
-struct tree {
-    struct node {
-        T data;
-        node *left = nullptr, *right = nullptr;
-
-        bool isleaf() const {
-            return left == nullptr && right == nullptr;
-        }
-    };
-
-    // List of unique pointers to tree nodes. This is useful to free memory when the tree is converted to DAG 
-    vector<unique_ptr<node>> nodes;
-    
-    // Creates and returns new node updating the nodes vector
-    node* make_node() {
-        nodes.emplace_back(make_unique<node>());
-        return nodes.back().get();
-    }
-
-    node *root = nullptr;
-
-    friend void swap(tree& t1, tree& t2) {
-        swap(t1.root, t2.root);
-    }
-
-    // Recursive function to copy a tree. This is required by the copy constructor
-    node *make_copy_rec(node *n) {
-        if (n == nullptr)
-            return nullptr;
-        node *nn = make_node();
-        nn->data = n->data;
-        nn->left = make_copy_rec(n->left);
-        nn->right = make_copy_rec(n->right);
-        return nn;
-    }
-
-    tree() {}
-
-    // Copy constructor
-    tree(const tree& t) {
-        root = make_copy_rec(t.root);
-    }
-    tree(tree&& t) {
-        swap(root, t.root);
-    }
-    tree& operator=(tree t) {
-        swap(*this, t);
-        return *this;
-    }
-
-    node* make_root() { return root = make_node(); }
-
-    template<typename fn>
-    void preorder_rec(node *n, fn& f, int i) {
-        if (n == nullptr)
-            return;
-        node *left = n->left, *right = n->right;
-        f(n, i);
-        preorder_rec(left, f, i + 1);
-        preorder_rec(right, f, i + 1);
-    }
-
-    template<typename fn>
-    void preorder(fn& f) {
-        preorder_rec(root, f, 0);
-    }
-
-    void clear() {
-        nodes.clear();
-        root = nullptr;
-    }
-};
-
-// Condition or action
-struct conact {
-    enum class type { CONDITION, ACTION };
-
-    type t;
-    // CONDITION
-    string condition;
-    // ACTION
-    uint action = 0; // List of actions (bitmapped)
-    uint next = 0;
-
-    vector<uint> actions() const {
-        vector<uint> a;
-        uint uAction = action;
-        uint nAction = 1;
-        while (uAction != 0) {
-            if (uAction & 1)
-                a.push_back(nAction);
-            uAction >>= 1;
-            nAction++;
-        }
-        return a;
-    }
-
-    bool operator==(const conact& other) const {
-        if (t != other.t)
-            return false;
-        if (t == type::CONDITION)
-            return condition == other.condition;
-        else
-            return (action & other.action) && next == other.next;
-    }
-    bool operator!=(const conact& other) const {
-        return !(*this == other);
-    }
-};
+using ltree = tree<conact>;
 
 void CreateTree_rec(tree<conact>& t, tree<conact>::node *n, const rule_set& rs, const VHyperCube &hcube, const VIndex &idx) {
     VNode node = hcube[idx];
@@ -158,42 +51,52 @@ tree<conact> CreateTree(const rule_set& rs, const VHyperCube &hcube) {
 
 
 // Checks if two subtrees 'n1' and 'n2' are equal or not 
-template <typename T>
-bool equal_trees(typename const tree<T>::node* n1, typename const tree<T>::node* n2) {
+bool equivalent_trees(const tree<conact>::node* n1, const tree<conact>::node* n2) {
     if (n1->data != n2->data)
         return false;
 
     if (n1->isleaf())
         return true;
     else
-        return equal_trees<T>(n1->left, n2->left) && equal_trees<T>(n1->right, n2->right);
+        return equivalent_trees(n1->left, n2->left) && equivalent_trees(n1->right, n2->right);
 }
 
-template <typename T>
-void find_and_link(typename tree<T>::node* n1, typename tree<T>::node* n2) {
+void intersect_leaves(tree<conact>::node* n1, tree<conact>::node* n2) {
+    if (n1->isleaf()) {
+        n2->data.action = n1->data.action &= n2->data.action;
+    }
+    else {
+        intersect_leaves(n1->left, n2->left);
+        intersect_leaves(n1->right, n2->right);
+    }
+}
+
+void find_and_link(tree<conact>::node* n1, tree<conact>::node* n2) {
     if (n2->isleaf() || n1 == n2)
         return;
 
-    if (n1 != n2->left && equal_trees<T>(n1, n2->left))
+    if (n1 != n2->left && equivalent_trees(n1, n2->left)) {
+        intersect_leaves(n1, n2->left);
         n2->left = n1;
-    if (n1 != n2->right && equal_trees<T>(n1, n2->right))
-        n2->right = n1;
+    }
 
-    find_and_link<T>(n1, n2->left);
-    find_and_link<T>(n1, n2->right);
+    if (n1 != n2->right && equivalent_trees(n1, n2->right)) {
+        intersect_leaves(n1, n2->right);
+        n2->right = n1;
+    }
+
+    find_and_link(n1, n2->left);
+    find_and_link(n1, n2->right);
 }
 
 
 // Recursive auxiliary function for the conversion of a tree into DAG
-template <typename T>
-void convert_tree_to_dag_rec(typename tree<T>::node *n, tree<T>& t) {
-    find_and_link<T>(n, t.root);
+void convert_tree_to_dag_rec(tree<conact>::node *n, tree<conact>& t) {
+    find_and_link(n, t.root);
 
     if (!n->isleaf()) {
-        if (!n->left->isleaf())
-            convert_tree_to_dag_rec<T>(n->left, t);
-        if (!n->right->isleaf())
-            convert_tree_to_dag_rec<T>(n->right, t);
+        convert_tree_to_dag_rec(n->left, t);
+        convert_tree_to_dag_rec(n->right, t);
     }
 }
 
@@ -240,9 +143,9 @@ struct tree_loader {
             n->data.t = conact::type::ACTION;
             do {
                 int action;
-                is >> action;
+                is >> action >> ws;
                 n->data.action |= 1 << (action - 1);
-            } while (is.peek() == ',');
+            } while (is.peek() == ',' && is.get());
         }
         else {
             // real node with branches
@@ -257,42 +160,77 @@ struct tree_loader {
     }
 };
 
-void print_node(tree<conact>::node *n, int i) 
-{
-    cout << string(i, '\t');
-    if (n->data.t == conact::type::CONDITION) {
-        cout << n->data.condition;
-    }
-    else {
-        cout << ". ";
-        auto v = n->data.actions();
-
-        cout << v[0];
-        for (size_t i = 1; i < v.size(); ++i) {
-            cout << "," << v[i];
+void Tree2OptimalDagRec(ltree& t, ltree::node* n, vector<ltree>& trees) {
+    ltree nt;
+    if (n->isleaf()) { 
+        // leaf with multiple action
+        vector<uint> actions_list = n->data.actions();
+        if (actions_list.size() > 1) {
+            for (size_t i = 0; i < actions_list.size() - 1; ++i) {
+                n->data.action = 1 << (actions_list[i] - 1);
+                t.CopyTo(nt);
+                Tree2OptimalDagRec(nt, nt.root, trees);
+            }
+            n->data.action = 1 << (actions_list[actions_list.size() - 1] - 1);
+            Tree2OptimalDagRec(t, t.root, trees);
         }
+        return;
     }
-    cout << "\n";
+
+
+    Tree2OptimalDagRec(t, n->left, trees);
+    Tree2OptimalDagRec(t, n->right, trees);
+
+    if (t.root == n) {
+        trees.push_back(t);
+    }
+
+    return;
+}
+
+// Converts a tree into dag minimizing the number of nodes (Note: this is "necessary" when the leaves of a tree contain multiple actions)
+void Tree2OptimalDag(ltree& t) {
+    vector<ltree> trees;
+    Tree2OptimalDagRec(t, t.root, trees);
+
+    for(size_t i = 0; i<trees.size(); ++i)
+    {
+        string s_txt = "tree_" + to_string(i) + ".txt";
+        string s_pdf = "tree_" + to_string(i) + ".pdf";
+        ofstream os(s_txt);
+        DrawDag(os, trees[i]);
+        os.close();
+        string cmd = "..\\tools\\dot\\dot -Tpdf " + s_txt + " -o " + s_pdf;
+        system(cmd.c_str());
+    }
+
 }
 
 int main()
 {
     tree_loader tl;
     tl.load_tree(ifstream("../doc/t1.txt"));
-    auto t1 = tl.t;
-    t1.preorder(print_node);
-    tl.load_tree(ifstream("../doc/t2.txt"));
-    auto t2 = tl.t;
-    t2.preorder(print_node);
+    ltree t1 = tl.t;
 
-    bool b1 = equal_trees<conact>(t1.root, t2.root);
-    bool b2 = equal_trees<conact>(t1.root, t1.root);
-    bool b3 = equal_trees<conact>(t2.root, t2.root);
-    bool b4 = equal_trees<conact>(t2.root, t1.root);
+    Tree2OptimalDag(t1);
+
+
+    {
+        ofstream os("tree.txt");
+        DrawDag(os, t1);
+        os.close();
+        system("..\\tools\\dot\\dot -Tpdf tree.txt -o tree.pdf");
+    }
+
+    t1.preorder(print_node);
 
     convert_tree_to_dag(t1);
-
-    t1.preorder(print_node);
+    {
+        ofstream os("dag.txt");
+        DrawDag(os, t1);
+        os.close();
+        system("..\\tools\\dot\\dot -Tpdf dag.txt -o dag.pdf");
+    }
 
     return 0;
 
