9f85825a1b216bd1f2711f809dc702ad4d1cdccf
Federico Bolelli <lk_number1@hotmail.it>
2019-08-26 09:45:34 +0200

    Add documentation generator

---
+++ doc/doxygen/mainpages/home.h
@@ -0,0 +1,27 @@
+/**
+@mainpage Documentation
+
+@image html graphsgen.png
+
+GRAPHSGEN is the all encompassing graphs generator!
+
+@cond
+@section manual_user User Manual
+@li @subpage page_introduction
+@li @subpage page_topics
+@li @subpage page_libs
+@li @subpage page_class_cat
+@li @subpage overview_changes_since28
+@section manual_reference Reference
+@li @subpage group_class
+@li @subpage group_funcmacro
+@li @subpage page_constants
+@section manual_misc Other Information
+@li @subpage page_samples
+@li @subpage page_utils
+@li @subpage page_translations
+@li @subpage page_multiplatform
+@li @subpage page_copyright
+@endcond
+
+*/
