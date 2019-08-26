9aced9a4627dfbbf1e447b8e55e81caaf6dff123
Federico Bolelli <lk_number1@hotmail.it>
2019-08-26 12:06:50 +0200

    Add missing include

--- src/GRAPHSGEN/graphsgen.h
+++ src/GRAPHSGEN/graphsgen.h
@@ -2,11 +2,12 @@
 #define GRAPHSGEN_GRAPHSGEN_H_
 
 #include "conact_code_generator.h"
-#include "graph_code_generator.h"
-#include "output_generator.h"
-#include "hypercube.h"
 #include "conact_tree.h"
-#include "merge_set.h"
 #include "connectivity_graph.h"
+#include "drag_statistics.h"
+#include "graph_code_generator.h"
+#include "hypercube.h"
+#include "merge_set.h"
+#include "output_generator.h"
 
 #endif // !GRAPHSGEN_GRAPHSGEN_H_
