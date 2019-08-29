64a177d72266d329d43e9a648a3d198456650972
Federico Bolelli <lk_number1@hotmail.it>
2019-08-29 20:10:53 +0200

    Add missing includes

--- src/GRAPHSGEN/graphsgen.h
+++ src/GRAPHSGEN/graphsgen.h
@@ -5,11 +5,13 @@
 #include "conact_tree.h"
 #include "connectivity_graph.h"
 #include "drag_statistics.h"
+#include "drag2optimal.h"
 #include "forest2dag.h"
 #include "forest_statistics.h"
 #include "graph_code_generator.h"
 #include "hypercube.h"
 #include "merge_set.h"
 #include "output_generator.h"
+#include "tree2dag_identities.h"
 
 #endif // !GRAPHSGEN_GRAPHSGEN_H_
