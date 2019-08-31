904a486fdd9941b751f7bb2cca57f12c3c800d47
Federico Bolelli <lk_number1@hotmail.it>
2019-08-31 11:20:43 +0200

    Add leaves sets to simplify backtracking during the conversion of a DAG into optimal DAG

--- src/Labeling/DRAG/drag_main.cpp
+++ src/Labeling/DRAG/drag_main.cpp
@@ -67,12 +67,59 @@ int main()
     string tree_filename = algorithm_name + "_tree";
     DrawDagOnFile(tree_filename, t, false, true, false);
 
+    struct MagicOptimizer {
+        struct STreeProp {
+            string conditions_;
+            vector<ltree::node*> leaves_;
+            ltree::node* n_;
+
+            STreeProp& operator+=(const STreeProp& rhs) {
+                conditions_ += rhs.conditions_;
+                copy(begin(rhs.leaves_), end(rhs.leaves_), back_inserter(leaves_));
+                return *this;
+            }
+
+            bool equivalent(const STreeProp& rhs) {
+                if (conditions_ != rhs.conditions_)
+                    return false;
+                for (size_t i = 0; i < leaves_.size(); ++i)
+                    if (leaves_[i]->data.next != rhs.leaves_[i]->data.next
+                        ||
+                        (leaves_[i]->data.action & rhs.leaves_[i]->data.action) == 0)
+                        return false;
+                return true;
+            }
+        };
+        unordered_map<ltree::node*, STreeProp> np_;
+
+        STreeProp CollectStatsRec(ltree::node * n) {
+            auto it = np_.find(n);
+            if (it != end(np_))
+                return it->second;
+
+            STreeProp sp;
+            sp.n_ = n;
+            if (n->isleaf()) {
+                sp.conditions_ = ".";
+                sp.leaves_.push_back(n);
+            }
+            else {
+                sp.conditions_ = n->data.condition;
+                sp += CollectStatsRec(n->left);
+                sp += CollectStatsRec(n->right);
+            }
+
+            np_[n] = sp;
+            return sp;
+        }
+    };
+
     struct RemoveEqualSubtrees {
         std::unordered_map<std::string, ltree::node*> sp_; // string -> pointer
         std::unordered_map<ltree::node*, std::string> ps_; // pointer -> string
         uint nodes_ = 0, leaves_ = 0;
 
-        string T2D(ltree::node*& n) 
+        string T2D(ltree::node*& n)
         {
             // Did we already find this node?
             auto itps = ps_.find(n);
@@ -175,7 +222,7 @@ int main()
             }
         }
 
-        void GenerateAllTreesRec(int cur_leaf) 
+        void GenerateAllTreesRec(int cur_leaf)
         {
             if (cur_leaf == lma_.size()) {
                 // We have a tree without multiple actions
@@ -187,7 +234,7 @@ int main()
 
             auto action_bs = lma_[cur_leaf]->data.action;
             auto actions = lma_[cur_leaf]->data.actions(); // vector of actions ("uint")
-            
+
             for (size_t i = 0; i < actions.size(); ++i) {
                 bitset<128> bs;
                 bs.set(actions[i] - 1);
@@ -205,55 +252,179 @@ int main()
         }
     };
 
-    TLOG("Creating DRAG using equivalences",
-        std::cout << "\n";
-        auto t2 = t;
-        RemoveEqualSubtrees sc;
-        sc.T2D(t2.root);
-        DrawDagOnFile("RemoveEqualSubtrees", t2, true);
-        std::cout << "After equal subtrees removal: nodes = " << sc.nodes_ << " - leaves = " << sc.leaves_ << "\n";
+    // Union-find (UF)
+    class UF {
+        // Maximum number of labels (included background) = 2^(sizeof(unsigned) x 8)
+    public:
+        UF(unsigned length) : length_{ length } {
+            P_ = new unsigned[length];
+            iota(P_, P_ + length, 0);
+        }
+        ~UF() {
+            delete[] P_;
+        }
+        unsigned GetSet(unsigned index) {
+            return P_[index];
+        }
 
-        FindOptimalDrag c(t2);
-        c.GenerateAllTrees();
-        DrawDagOnFile("FindOptimalDrag", c.best_tree_, true);
-        std::cout << "\n";
-    );
-    return 0;
+        unsigned Merge(unsigned i, unsigned j)
+        {
+            // FindRoot(i)
+            while (P_[i] < i) {
+                i = P_[i];
+            }
 
-    // 2a) Convert Optimal Decision Tree into Directed Rooted Acyclic Graph
-    //     using a exhaustive strategy 
-    TLOG("Creating DRAG using identites",
-        Tree2DagUsingIdentities(t);
-    );
-    string drag_filename = algorithm_name + "_drag_identities";
-    DrawDagOnFile(drag_filename, t, true);
-    PrintStats(t);
+            // FindRoot(j)
+            while (P_[j] < j) {
+                j = P_[j];
+            }
 
-    //string odrag_filename = algorithm_name + "_optimal_drag.txt";
-    //if (!LoadConactDrag(t, odrag_filename)) {
-    //    TLOG("Computing optimal DRAG\n",
-    //        Dag2OptimalDag(t);
-    //    );
-    //    WriteConactDrag(t, odrag_filename);
-    //}
-    //string optimal_drag_filename = algorithm_name + "_optimal_drag";
-    //DrawDagOnFile(optimal_drag_filename, t, true);
-    //PrintStats(t);
+            if (i < j)
+                return P_[j] = i;
+            return P_[i] = j;
+        }
 
-    // 2b) Convert Optimal Decision Tree into Directed Rooted Acyclic Graph
-    //     using a heuristic
-    // TODO
-    
-    // 3) Generate the C++ source code for the ODT
-    GenerateDragCode(algorithm_name, t);
+        unsigned Flatten()
+        {
+            unsigned k = 1;
+            for (unsigned i = 1; i < length_; ++i) {
+                if (P_[i] < i) {
+                    P_[i] = P_[P_[i]];
+                }
+                else {
+                    P_[i] = k;
+                    k = k + 1;
+                }
+            }
+            return k;
+        }
 
-    // 4) Generate the C++ source code for pointers, 
-    // conditions to check and actions to perform
-    pixel_set block_positions{
-          { "P", {-2, -2} },{ "Q", {+0, -2} },{ "R", {+2, -2} },
-          { "S", {-2, +0} },{ "x", {+0, +0} }
+    private:
+        unsigned *P_;
+        unsigned length_;
     };
-    GeneratePointersConditionsActionsCode(algorithm_name, rs, block_positions);
 
+    auto t2 = t;
+    RemoveEqualSubtrees sc;
+    sc.T2D(t2.root);
+
+    MagicOptimizer mo;
+    mo.CollectStatsRec(t2.root);
+    vector<MagicOptimizer::STreeProp> trees;
+    for (const auto& x : mo.np_)
+        trees.push_back(x.second);
+
+    UF uf(trees.size());
+    for (uint i = 0; i < trees.size(); ++i) {
+        for (uint j = i + 1; j < trees.size(); ++j) {
+            if (trees[i].equivalent(trees[j])) {
+                uf.Merge(i, j);
+            }
+        }
+    }
+    auto nsets = uf.Flatten();
+
+    // This map stores for each leaf the set (integer number given by 
+    // the previous UF) of trees from which it belongs to.
+    map<ltree::node*, std::set<uint>> lt;
+
+    // The following nested loops populate the lt map. 
+    // For each tree
+    for (uint i = 0; i < trees.size(); ++i) {
+        // For each leaf 
+        auto& t = trees[i];
+        for (uint j = 0; j < t.leaves_.size(); ++j) {
+            auto& l = t.leaves_[j];
+            lt[l].insert(uf.GetSet(i));
+        }
+    }
+
+    std::vector<pair<ltree::node*, std::set<uint>>> vlt;
+    for (const auto& x : lt) {
+        vlt.push_back(make_pair(x.first, x.second));
+    }
+
+    //// FindOptimalDrag here is exploited just to find the vector of leaves with
+    //// multiple actions, no more. 
+    //FindOptimalDrag fod(t2);
+    //auto& leaves = fod.lma_; // vector of leaves with multiple actions
+
+    UF ufl(vlt.size());
+    // For each leaf look to all the other leaves and if they belong both
+    // to at least one (same) set of trees they must be in the same uf class
+    // all the leaves belonging to the same set will be used to run the 
+    // backtracking approach of the FindOptimalDrag (once per set separately).
+    for (uint i = 0; i < vlt.size(); ++i) {
+        if (vlt[i].first->data.actions().size() > 1) {
+            for (uint j = i + 1; j < vlt.size(); ++j) {
+                if (vlt[j].first->data.actions().size() > 1) {
+                    std::vector<uint> intersection(nsets);
+                    // This can be done by hand breaking the loop after finding the first intersection
+                    auto it = std::set_intersection(begin(vlt[i].second), end(vlt[i].second), begin(vlt[j].second), end(vlt[j].second), begin(intersection));
+                    std::cout << " ";
+                    if (it != begin(intersection)) {
+                        // Non empty intersection
+                        ufl.Merge(i, j);
+                    }
+                }
+            }
+        }
+    }
+    auto nsets_two = ufl.Flatten();
+
+
+
+    return 0;
+    /*
+        TLOG("Creating DRAG using equivalences",
+            std::cout << "\n";
+            auto t2 = t;
+            RemoveEqualSubtrees sc;
+            sc.T2D(t2.root);
+            DrawDagOnFile("RemoveEqualSubtrees", t2, true);
+            std::cout << "After equal subtrees removal: nodes = " << sc.nodes_ << " - leaves = " << sc.leaves_ << "\n";
+
+            FindOptimalDrag c(t2);
+            c.GenerateAllTrees();
+            DrawDagOnFile("FindOptimalDrag", c.best_tree_, true);
+            std::cout << "\n";
+        );
+        return 0;
+
+        // 2a) Convert Optimal Decision Tree into Directed Rooted Acyclic Graph
+        //     using a exhaustive strategy
+        TLOG("Creating DRAG using identites",
+            Tree2DagUsingIdentities(t);
+        );
+        string drag_filename = algorithm_name + "_drag_identities";
+        DrawDagOnFile(drag_filename, t, true);
+        PrintStats(t);
+
+        //string odrag_filename = algorithm_name + "_optimal_drag.txt";
+        //if (!LoadConactDrag(t, odrag_filename)) {
+        //    TLOG("Computing optimal DRAG\n",
+        //        Dag2OptimalDag(t);
+        //    );
+        //    WriteConactDrag(t, odrag_filename);
+        //}
+        //string optimal_drag_filename = algorithm_name + "_optimal_drag";
+        //DrawDagOnFile(optimal_drag_filename, t, true);
+        //PrintStats(t);
+
+        // 2b) Convert Optimal Decision Tree into Directed Rooted Acyclic Graph
+        //     using a heuristic
+        // TODO
+
+        // 3) Generate the C++ source code for the ODT
+        GenerateDragCode(algorithm_name, t);
+
+        // 4) Generate the C++ source code for pointers,
+        // conditions to check and actions to perform
+        pixel_set block_positions{
+              { "P", {-2, -2} },{ "Q", {+0, -2} },{ "R", {+2, -2} },
+              { "S", {-2, +0} },{ "x", {+0, +0} }
+        };
+        GeneratePointersConditionsActionsCode(algorithm_name, rs, block_positions);
+    */
     return EXIT_SUCCESS;
 }
