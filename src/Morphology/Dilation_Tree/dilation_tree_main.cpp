81e5d75b70a234489d5d123a2521fb4833b86796
Federico Bolelli <federico.bolelli@unimore.it>
2020-12-23 13:39:28 +0100

    Add missing morphology targets

---
+++ src/Morphology/Dilation_Tree/dilation_tree_main.cpp
@@ -0,0 +1,42 @@
+// Copyright (c) 2020, the GRAPHGEN contributors, as
+// shown by the AUTHORS file. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+#include "graphgen.h"
+
+#include "dilation_ruleset.h"
+
+using namespace std;
+
+int main()
+{
+    string algorithm_name = "Dilation3x3_Tree";
+    string mask_name = "kernel3x3";
+
+    conf = ConfigData(algorithm_name, mask_name);
+
+    DilationRS d_rs;
+    auto rs = d_rs.GetRuleSet();
+
+    // Call GRAPHGEN:
+    // 1) Load or generate Optimal Decision Tree based on Erosion 3x3 mask
+    BinaryDrag<conact> bd = GetOdt(rs);
+
+    // 2) Draw the generated tree to pdf
+    string tree_filename = algorithm_name + "_tree";
+    DrawDagOnFile(tree_filename, bd);
+
+    // 3) Generate the C++ source code for the ODT
+    ofstream os(conf.treecode_path_);
+    if (os) {
+        GenerateDragCode(os, bd);
+    }
+    
+    // 4) Generate the C++ source code for pointers,
+    // conditions to check and actions to perform
+    // Not implemented for morphology 
+
+    return EXIT_SUCCESS;
+}
